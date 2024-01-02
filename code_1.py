3ed32072fe5b486b6f5d9750bbc0eb857b9ad6ac
William Logan <wiloj2018@gmail.com>
2024-01-02 19:49:49 +0000

    test

---
+++ code_1.py
@@ -0,0 +1,154 @@
+# Dependencies
+# https://stackoverflow.com/questions/65240392/how-can-i-use-spotdl-inside-python
+import os
+from os import system
+import random
+from spotdl import Spotdl
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotdl.utils.ffmpeg import download_ffmpeg
+import glob
+import time
+from dotenv import load_dotenv
+from pydub import AudioSegment
+from pydub.playback import play
+from threading import Thread
+
+
+
+load_dotenv()
+
+client_id = os.environ.get("SPOTIPY_CLIENT_ID")
+client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
+
+
+spotify = Spotify(client_credentials_manager=SpotifyClientCredentials())
+spotdl = Spotdl(client_id,
+                client_secret)
+
+# Grab ten random songs that are "metal", return their names
+offset = random.randint(0, 1000)
+query = "genre:metal"
+track_urls = [[x["name"], x["external_urls"]["spotify"]]
+              for x in spotify.search(query, offset=offset)["tracks"]["items"]]
+
+# Search for the song
+print(track_urls)
+songs = spotdl.search(random.choice(track_urls))
+
+# Attempt to download the song
+#results = spotdl.download_songs(songs)
+
+song = spotdl.download(songs[0])
+
+#find the song name of the mp3
+mp3_files = glob.glob("*.mp3")
+print(mp3_files)
+
+#need to get the name without the [" "] so it can be played using playsound
+song_name = mp3_files[0].replace("[", "").replace("]", "").replace(" ","")
+print(song_name)
+
+file = mp3_files[0]
+
+split = song_name.partition("-")
+track_name = split[0]
+artist = split[2]
+artist_name = artist.partition(".")
+artist_name = artist_name[0]
+print(track_name,artist_name)
+
+print(file)
+
+os.rename(file,"cook.mp3")
+
+def play_audio():
+  playing = AudioSegment.from_mp3("cook.mp3")
+  ten_seconds = 10 * 1000
+  first_10_seconds = playing[:ten_seconds]
+  play(first_10_seconds)
+
+def threads():
+  Thread(target=play_audio, daemon=True).start()
+
+def deleting():
+  os.remove("cook.mp3")
+  exit()
+
+
+from tkinter import (
+  Toplevel,
+  Label,
+  Button,
+  Entry,
+  Tk
+)
+import tkinter as tkinter
+
+def open_window():
+  window = Toplevel()
+  window.geometry('500x300')
+  window.title('Guess the artist or song')
+  
+  Button(window,text='Close',command=deleting).pack()
+  Button(window,text='Play',command=threads).pack()
+  Label(window,text="Guess:").pack()
+  value_entry = Entry(window)
+  value_entry.pack()
+
+  # Define our answers
+  funcid = None
+  answers = [track_name, artist_name]
+
+  # Called whenever we press the enter key
+  def key_pressed(key: tkinter.Event):
+    # Grab the user input
+    value = str(value_entry.get()).strip()
+
+    # Check their answer is within the answers array
+    if value in answers:
+      Label(window, text="correct").pack()
+      
+
+      # Clear the text they entered
+      value_entry.delete(0, tkinter.END)
+    else:
+      # Clear the text they entered
+      value_entry.delete(0, tkinter.END)
+      Label(window, text="incorrect").pack()
+
+
+  
+  # Listen to when user presses enter
+  funcid = window.bind("<Return>",key_pressed)
+
+  window.bind('<Return>', key_pressed)
+  window.grab_set()
+
+if __name__ == "__main__":
+  app = Tk()
+  app.geometry('300x200')
+  app.title('Music Quiz ?')
+  Button(app,text='Start Game',command=open_window).pack()
+  Button(app,text='Exit',command=app.destroy).pack()
+  app.mainloop()
+
+
+
+
+
+
+
+
+#guess = input("Enter the song or artist name without spaces and caps:\n")
+#if guess == track_name or guess == artist_name:
+#  print("Correct!")
+#  os.remove("cook.mp3")
+#  exit()
+#else:
+#  print("Incorrect!")
+#  os.remove("cook.mp3")
+#  exit()
+
+
+
